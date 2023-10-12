80a8ab47b711adaef789a0cd6368183ccc8dcf3f
Isaac Sarte <sarteisaac09@gmail.com>
2023-10-13 07:46:06 +0800

    initial commit

---
+++ pages/see.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const See = () => {
+  return (
+    <div>See</div>
+  )
+}
+
+export default See;
---
+++ src/modules/binocular.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Binocular = () => {
+  return (
+    <div>Binocular</div>
+  )
+}
+
+export default Binocular;
