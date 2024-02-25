c89eb9262e692ffa2a7044d08d6ef5edaa6662b8
Isaac Sarte <sarteisaac09@gmail.com>
2024-02-25 21:06:34 +0800

    initial experiences page

--- src/modules/experiences.jsx
+++ src/modules/experiences.jsx
@@ -1,19 +1,66 @@
 "use client"
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import React, { Suspense } from 'react';
 import GoBack from './nav/go-back';
+import cn from 'classnames';
+import { experiencesList } from '@/utils/experiences/experiences';
 
 const Experiences = () => {
-  return (
-    <Suspense fallback={null}>
-        <AnimatePresence>
-            <GoBack />
-            <div className="">
-                Experiences
-            </div>
-        </AnimatePresence>
-    </Suspense>
-  )
+    const staggerDelay = 0.25;
+    const variants = {
+        hidden: { opacity: 0, x: '-10%' },
+        hiddenOdd: { opacity: 0, x: '10%' },
+        visible: { opacity: 1, x: '0%' },
+        reverseVisible: { opacity: 1, x: '0%' },
+    };
+
+    return (
+        <Suspense fallback={null}>
+            <AnimatePresence>
+                <GoBack />
+                <div className="grid grid-cols-3 gap-4 px-12">
+                    {experiencesList.map((item, index) => (
+                        <motion.div
+                            key={index}
+                            className={cn(
+                                'bg-[#fbfbfb] text-black p-8',
+                                {
+                                    'col-span-2': index % 4 === 0 || index % 4 === 3,
+                                    'col-span-1 flex flex-col': index % 4 !== 0 && index % 4 !== 3,
+                                }
+                            )}
+                            initial={index % 2 === 0 ? 'hidden' : 'hiddenOdd'}
+                            animate={index % 2 === 0 ? 'visible' : 'reverseVisible'}
+                            variants={variants}
+                            transition={{ duration: 1, delay: index * staggerDelay, type: 'spring', bounce: 0.25 }}
+                        >
+                            <span className="text-2xl">{item.title}</span>
+                            {index % 4 === 0 || index % 4 === 3 ? (
+                                <div className="flex justify-between mt-4">
+                                    <div className="flex flex-col">
+                                        <span>Description</span>
+                                        <span>Link</span>
+                                    </div>
+                                    <span>Image</span>
+                                </div>
+                            ) : (
+                                <div>
+                                    <div className="flex justify-center mt-4">
+                                        <span>Image</span>
+                                    </div>
+
+                                    <div className="flex flex-col mt-4">
+                                        <span>Description</span>
+                                        <span>Link</span>
+                                    </div>
+                                </div>
+                            )}
+                        </motion.div>
+                    ))}
+                </div>
+            </AnimatePresence>
+        </Suspense>
+    )
 }
 
 export default Experiences
---
+++ src/utils/experiences/experiences.js
@@ -0,0 +1,8 @@
+export const experiencesList = [
+    { id: 0, title: 'Mosaic Solutions' },
+    { id: 1, title: 'Manila Fashion Festival' },
+    { id: 2, title: 'Vawchoo' },
+    { id: 3, title: 'JNCE Medical and Diagnostic Clinic' },
+    { id: 4, title: 'Srixon' },
+    { id: 5, title: 'Magic Inc' },
+]
