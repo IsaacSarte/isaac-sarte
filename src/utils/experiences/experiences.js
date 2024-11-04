492ccedeaf2a14a0b34af86f35af328ddf532a61
Isaac Sarte <82153590+IsaacSarte@users.noreply.github.com>
2024-11-04 22:43:39 +0800

    change year

--- src/utils/experiences/experiences.js
+++ src/utils/experiences/experiences.js
@@ -10,6 +10,6 @@ export const experiencesList = [
     { id: 1, title: 'Manila Fashion Festival (2023)', image: exp2, time: "Freelance", desc: "Taking Filipino Fashion to the Global Scene", link: 'https://mff-official.com/' },
     { id: 2, title: 'Vawchoo (2023)', image: exp3, time: "Freelance", desc: "Your one-stop beauty hub", link: 'https://vawchoo.com/' },
     { id: 3, title: 'JNCE Medical and Diagnostic Clinic (2022)', image: exp4, time: "Freelance", desc: "Envisions to become a high-standard provider of pre-employment medical services", link: 'https://jnce-landing.vercel.app/' },
-    { id: 4, title: 'Magic Inc (2023 - Present)', image: exp5, time: "Full Time", desc: "Get an incredible Assistant. Get Magic.", link: 'https://getmagic.com/' },
+    { id: 4, title: 'Magic Inc (2023 - 2024)', image: exp5, time: "Full Time", desc: "Get an incredible Assistant. Get Magic.", link: 'https://getmagic.com/' },
     { id: 5, title: 'Proudcloud (2024 - Present)', image: exp6, time: "Part Time", desc: "Accelerate Your Product's Time-to-Market", link: 'https://www.proudcloud.io/' },
 ]
